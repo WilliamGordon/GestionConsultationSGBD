ac08720a8292023acb5e10c86960bdc2e873f19f
WilliamGordon <william.wauters@outlook.com>
2021-02-22 16:32:40 +0100

    DAL With Entity Framework using EDMX

---
+++ Project/2_developement/GestionConsultationAPP/ConsoleTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new GestionConsultationEntities();
+            var spec = context.Specialites.ToList();
+
+            foreach (var sp in spec)
+            {
+                Console.WriteLine(sp.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
